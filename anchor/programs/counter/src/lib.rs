518b8c076616f167bb94f386f7ca3d7ce738de24
yash <yashvikram8250@gmail.com>
2025-06-04 11:06:39 +0530

    changes

--- anchor/programs/counter/src/lib.rs
+++ anchor/programs/counter/src/lib.rs
@@ -8,5 +8,24 @@ declare_id!("FqzkXZdwYjurnUKetJCAvaUw5WAqbwzU6gZEwydeEfqS");
 pub mod voting{
     use super::*;
 
-    pub fn
+    pub fn initialize_poll(ctx: Context<InitailizePoll>, poll_id: u64)->Result<()>{
+        
+    }
+
+    #[derive[Accounts]]
+    pub struct InitailizePoll<'info>{
+        #[account(mut)]
+        pub signer: Signer<'info>,
+        #[account()]
+        pub poll: Account<'info, Poll>,
+    }
+
+    #[account]
+    pub struct Poll{
+        pub poll_id: u64,
+        pub description: String,
+        pub poll_start: u64,
+        pub poll_end: u64,
+        pub candidate_amount: u64
+    }
 }
