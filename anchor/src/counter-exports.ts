0543fa90938c6650360a504cdc03d85005493fb1
yash <yashvikram8250@gmail.com>
2025-06-07 12:50:24 +0530

    completed smart contract

--- anchor/src/counter-exports.ts
+++ anchor/src/counter-exports.ts
@@ -1,8 +1,8 @@
 // Here we export some useful types and functions for interacting with the Anchor program.
 import { AnchorProvider, Program } from '@coral-xyz/anchor'
 import { Cluster, PublicKey } from '@solana/web3.js'
-import CounterIDL from '../target/idl/counter.json'
-import type { Counter } from '../target/types/counter'
+import CounterIDL from '../target/idl/voting.json'
+import type { Counter } from '../target/types/voting'
 
 // Re-export the generated IDL and type
 export { Counter, CounterIDL }
